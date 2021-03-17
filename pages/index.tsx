6bc2a314dcad06908174f7769ac26b8b3a2fb149
polvora-fosforo <contaparacodigo999@gmail.com>
2021-03-17 14:18:27 -0300

    [index.tsx]
    -> creating home structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,63 @@
+import React, { useState } from 'react';
+
 export default function Home() {
-  return(
-    <h1>This is Water Accountant!</h1>
-  )
+  const [isLoginForm, setisLoginForm] = useState(true);
+
+  return (
+    <div>
+      <h1>Water Accountant</h1>
+
+      <form>
+        <input
+          type="radio"
+          id="login"
+          name="formType"
+          defaultChecked={isLoginForm}
+          value={1}
+          onChange={() => setisLoginForm(true)}
+        />
+        <input
+          type="radio"
+          id="register"
+          name="formType"
+          defaultChecked={!isLoginForm}
+          value={0}
+          onChange={() => setisLoginForm(false)}
+        />
+      </form>
+
+      {isLoginForm ? (
+        <form>
+          <label htmlFor="email">
+            E-mail
+            <input type="email" id="email" />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input type="password" id="password" />
+          </label>
+
+          <input type="button" value="Login" />
+        </form>
+      ) : (
+        <form>
+          <label htmlFor="email">
+            E-mail
+            <input type="email" id="email" />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input type="password" id="password" />
+          </label>
+          <label htmlFor="confirm-password">
+            Confirm Password
+            <input type="password" id="confirm-password" />
+          </label>
+
+          <input type="button" value="Register" />
+        </form>
+      )}
+
+    </div>
+  );
 }

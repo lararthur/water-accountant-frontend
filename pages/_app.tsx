ad0559c7e0029f60d852bb87e26362ff1b6d4738
polvora-fosforo <contaparacodigo999@gmail.com>
2021-03-17 11:06:53 -0300

    [eslint, _app.tsx]
    -> disabling the 'react/jsx-props-no-spreading' rule (inline) on _app.tsx
    -> creating interface for _app.tsx props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,12 @@
+import React from 'react';
+
+interface MyAppProps {
+  Component: () => any;
+  pageProps: {};
+}
+
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: MyAppProps) {
+  /* eslint-disable react/jsx-props-no-spreading */
+  return <Component {...pageProps} />;
 }

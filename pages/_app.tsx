9f4d8261a5508d11e742fd653012700b0a8b54de
polvora-fosforo <contaparacodigo999@gmail.com>
2021-03-17 15:03:02 -0300

    [app.tsx]
    -> adding custom font via google fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import '../styles/global.scss';
+import '../styles/reset.scss';
+import Head from 'next/head';
 
 interface MyAppProps {
   Component: () => any;
@@ -8,5 +11,13 @@ interface MyAppProps {
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: MyAppProps) {
   /* eslint-disable react/jsx-props-no-spreading */
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Questrial&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }

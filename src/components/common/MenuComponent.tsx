2eafb0b414c9ec50b955778503a08f50e7af132b
polvora-fosforo <contaparacodigo999@gmail.com>
2021-03-29 17:01:03 -0300

    menu list appearance

--- src/components/common/MenuComponent.tsx
+++ src/components/common/MenuComponent.tsx
@@ -3,14 +3,31 @@ import styles from '../../styles/menu.module.scss';
 
 export default function MenuComponent(): JSX.Element {
   return (
-    <section className={styles.menuBar}>
-      <div className={styles.menuIcon}>
-        <img
-          className={styles.menuIcon__icon}
-          src="icons/menu.svg"
-          alt="menu"
-        />
-      </div>
-    </section>
+    <>
+      <section className={styles.menuBar}>
+        <div className={styles.menuIcon}>
+          <img
+            className={styles.menuIcon__icon}
+            src="icons/menu.svg"
+            alt="menu"
+          />
+        </div>
+      </section>
+
+      <section className={styles.menuBackground}>
+        <div className={styles.menuCover}>
+
+          <nav className={styles.menu}>
+            <ul>
+              <li>Home</li>
+              <li>Add custom measure</li>
+              <li>My Info</li>
+              <li>Logout</li>
+            </ul>
+          </nav>
+
+        </div>
+      </section>
+    </>
   );
 }

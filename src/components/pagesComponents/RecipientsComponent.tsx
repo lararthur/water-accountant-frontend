f824b2bca6337e7510dcfd3e212ec96f1680e12b
polvora-fosforo <contaparacodigo999@gmail.com>
2021-03-30 13:55:22 -0300

    [icons, RecipientsComponent]
    -> changing and adding icons previouly forgotten
    -> initial style for the recipientsComponent

--- src/components/pagesComponents/RecipientsComponent.tsx
+++ src/components/pagesComponents/RecipientsComponent.tsx
@@ -3,6 +3,30 @@ import styles from '../../styles/recipients.module.scss';
 
 export default function RecipientsComponent():JSX.Element {
   return (
-    <section className={styles.recipientsContainer}>RecipientsComponent</section>
+    <section className={styles.recipientsContainer}>
+      <div className={styles.recipientsBox}>
+        <div>
+          You don’t have any drinking recipient added to your shortcut...
+          You can do it by clicking the button to the right!
+          <img
+            src="icons/arrow-right.svg"
+            alt="Click the button to the right"
+            className={styles.recipientsBox__arrow}
+          />
+        </div>
+      </div>
+
+      <button
+        type="button"
+        className={styles.drinkOrAddButton}
+      >
+        <img
+          src="icons/add.svg"
+          alt="Drink or add custom measure"
+          className={styles.drinkOrAddButton__icon}
+        />
+        Drink/add custom measure
+      </button>
+    </section>
   );
 }

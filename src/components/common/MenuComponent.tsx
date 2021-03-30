6b27690bf4f6fd71dcb3ae4c39c05e6432505526
polvora-fosforo <contaparacodigo999@gmail.com>
2021-03-30 10:27:25 -0300

    [menuComponent]
    -> trying to figure out how menu list can be closed or opened with touch
    -> still bogus.

--- src/components/common/MenuComponent.tsx
+++ src/components/common/MenuComponent.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from '../../styles/menu.module.scss';
 
 export default function MenuComponent(): JSX.Element {
-  const checkMenuPosition = (e) => {
-    console.log(e);
-    // const chagesOn = (scrollWidth / 2) - (offsetWidth / 4)
-    // if scrollLeft <= chagesOn ? closeMenu() : openMenu()
+  const menuBgRef = useRef();
+  /* menuBgRef.current && menuBgRef.current.scroll({
+    left: menuBgRef.current.offsetWidth,
+    behavior: 'smooth',
+  }); */
+
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  const moveMenuHandler = () => {
+    if (!isScrolling) {
+      setIsScrolling(true);
+    }
+  };
+
+  const dropMenuHandler = () => {
+    if (isScrolling) {
+      // const changesOn = (scrollWidth / 2) - (offsetWidth / 4)
+      // if scrollLeft <= chagesOn ? closeMenu() : openMenu()
+
+      const changesOn = (menuBgRef.current.scrollWidth / 2) - (menuBgRef.current.offsetWidth / 4);
+      if (menuBgRef.current.scrollLeft <= changesOn) {
+        console.log('close menu');
+
+        setTimeout(() => {
+          menuBgRef.current.scroll({
+            left: 0,
+            behavior: 'smooth',
+          });
+        }, 200);
+      } else {
+        console.log('open menu');
+
+        setTimeout(() => {
+          menuBgRef.current.scroll({
+            left: menuBgRef.current.offsetWidth,
+            behavior: 'smooth',
+          });
+        }, 200);
+      }
+
+      setIsScrolling(false);
+    }
   };
 
   return (
@@ -20,7 +58,16 @@ export default function MenuComponent(): JSX.Element {
         </div>
       </section>
 
-      <section onScroll={(e) => checkMenuPosition(e)} className={styles.menuBackground}>
+      <section
+        tabIndex={0}
+        role="switch"
+        /* @TODO: assign to 'aria-checked' the 'isMenuOpen' flag */
+        aria-checked="false"
+        ref={menuBgRef}
+        onTouchStart={moveMenuHandler}
+        onTouchEnd={dropMenuHandler}
+        className={styles.menuBackground}
+      >
         <div className={styles.menuCover}>
 
           <nav className={styles.menu}>

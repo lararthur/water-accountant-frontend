7374bd4c97925c4c5899df6d95fc66f9992fe97b
polvora-fosforo <contaparacodigo999@gmail.com>
2021-03-31 12:59:36 -0300

    creation of user-info and water-recipients pages (empty though)

---
+++ src/pages/user-info.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function UserInfo(): JSX.Element {
+  return (
+    <p>UserInfo</p>
+  );
+}
---
+++ src/pages/water-recipient.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function WaterRecipient(): JSX.Element {
+  return (
+    <p>Water Recipient</p>
+  );
+}
